8dc03426b048019a3a2663c73da139cb4e3d7e74
Santiago Mora <111421466+SantiagoMoraV@users.noreply.github.com>
2022-11-26 02:35:32 -0600

    Delete prueba.py

--- Sucesión_fibonacci/prueba.py
+++
@@ -1,7 +0,0 @@
-import timeit
-import cy_fibonacci.pyx
-
-py = timeit.timeit("fibonacci(30)", "from __main__ import fibonacci", number=30)
-
-cy = timeit.timeit("fibonacci(30)", "from cy_fibonacci import py_fibonacci", number=30)
-
